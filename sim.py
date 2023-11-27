1f145f24dde867909203d7ab45a6369d6fc21c36
Niklas Bieck <bieck.niklas.julius.uo@tut.jp>
2023-11-27 18:29:30 +0900

    Implement prediction step of filter and prepare for correction

--- sim.py
+++ sim.py
@@ -2,17 +2,23 @@ import pygame
 import random
 import numpy as np
 import math
+import icp
+
 
 class Config:
 
     def __init__(self):
 
-        self.vel = 300
+        self.vel = 300.
         self.angular_vel = math.pi
         self.sens_range = 200
         # noise takes std dev. set value using variance
         self.sens_noise = math.sqrt(40)
         self.landmarks = 50
+        self.particles = 200
+        # assumed noise in the movement model. As above, std dev
+        self.vel_noise = math.sqrt(30)
+        self.rot_vel_noise = math.sqrt(math.pi / 8)
 
 class Player:
 
@@ -129,6 +135,68 @@ class Sensor:
 
                 self.hits.append(SensorHit(np.linalg.norm(diff), math.atan2(-diff[1], diff[0]) - self.player.rot))
 
+class Particle:
+
+    def make_rand():
+        return Particle(np.array([random.uniform(0,1280), random.uniform(0,720)]), random.uniform(0, math.pi * 2))
+
+    def copy(other):
+        return Particle(np.copy(other.pos), other.rot)
+
+    def __init__(self, pos, rot):
+        self.pos = pos
+        self.rot = rot
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0,255,0), self.pos, 5, 1)
+        pygame.draw.line(screen, (0,255,0), self.pos, self.pos + [5 * -math.sin(self.rot), -5 * math.cos(self.rot)])
+
+class ParticleFilter:
+
+    def __init__(self, config):
+        self.config = config
+
+        self.particles = []
+        for _ in range(config.particles):
+            self.particles.append(Particle.make_rand())
+
+    def draw(self, screen):
+        for particle in self.particles:
+            particle.draw(screen)
+
+    def prediction(self, dt):
+        keys = pygame.key.get_pressed()
+        for particle in self.particles:
+            d = np.array([-math.sin(particle.rot), -math.cos(particle.rot)])
+
+            vel_noise = random.gauss(0, self.config.vel_noise)
+            rot_noise = random.gauss(0, self.config.rot_vel_noise)
+
+            vel = self.config.vel + vel_noise
+            rot_vel = self.config.angular_vel + rot_noise
+
+            if keys[pygame.K_w]:
+                particle.pos += d * vel * dt
+            if keys[pygame.K_s]:
+                particle.pos -= d * vel * dt
+
+            if keys[pygame.K_a]:
+                particle.rot += rot_vel * dt
+            if keys[pygame.K_d]:
+                particle.rot -= rot_vel * dt
+
+            if particle.rot < 0:
+                particle.rot += math.pi * 2
+            elif particle.rot > math.pi * 2:
+                particle.rot -= math.pi * 2
+
+    def update(self, dt):
+        self.prediction(dt)
+
+        #correction
+
+        #resampling
+
 class Sim:
 
     def __init__(self, screen, clock):
@@ -140,6 +208,7 @@ class Sim:
         self.landmarks = []
         self.sensor = None
         self.config = Config()
+        self.filter = None
 
     def __init(self):
         self.player = Player(self.config)
@@ -147,6 +216,7 @@ class Sim:
         self.landmarks.clear()
         for _ in range(self.config.landmarks):
             self.landmarks.append(Landmark())
+        self.filter = ParticleFilter(self.config)
 
     def __events(self):
         for event in pygame.event.get():
@@ -165,6 +235,7 @@ class Sim:
 
         self.player.update(self.dt)
         self.sensor.update(self.landmarks)
+        self.filter.update(self.dt)
 
     def __draw(self):
         self.screen.fill("black")
@@ -172,6 +243,7 @@ class Sim:
         for lm in self.landmarks:
             lm.draw(self.screen)
         self.sensor.draw(self.screen)
+        self.filter.draw(self.screen)
         self.player.draw(self.screen)
 
         pygame.display.flip()
